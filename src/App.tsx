eaf2b8f96d8cc06ed113fa9fba45050a7ff036f1
cechua <chuacelc08@gmail.com>
2024-07-10 01:21:09 +0800

    Initial files - Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './App.css';
-import CustomButton from './components/customButton/CustomButton';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import JobsPage from './components/page/JobsPage';
+import ApplicationsPage from './components/page/ApplicationsPage';
+import { Header } from './components/header/Header';
 
 function App() {
-  return <CustomButton />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="jobs" element={<JobsPage />} />
+        <Route path="applications" element={<ApplicationsPage />} />
+        <Route path="*" element={<Navigate to="/jobs" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Container, Group, Button, Autocomplete } from '@mantine/core';
+import classes from './header.module.css';
+import { BsSearch } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+
+const links = [
+  { link: '/jobs', label: 'Jobs' },
+  { link: '/applications', label: 'Applications' },
+];
+
+export function Header() {
+  //const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+
+  const items = links.map((link) => (
+    <Link
+      to={link.link}
+      key={link.label}
+      className={classes.link}
+      data-active={active === link.link || undefined}
+      onClick={() => {
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </Link>
+  ));
+
+  return (
+    <header className={classes.header}>
+      <Container className={classes.inner} fluid>
+        <h1 className={classes.headerStart}>JobsJobsJOBS</h1>
+        <Group gap={20} visibleFrom="xs" className={classes.headerCenter}>
+          <Autocomplete
+            className={classes.search}
+            placeholder="Search"
+            leftSection={<BsSearch />}
+            data={[
+              'React',
+              'Angular',
+              'Vue',
+              'Next.js',
+              'Riot.js',
+              'Svelte',
+              'Blitz.js',
+            ]}
+            visibleFrom="xs"
+          />
+          <Group>{items}</Group>
+        </Group>
+        <Group
+          justify="center"
+          px="md"
+          visibleFrom="xs"
+          className={classes.headerEnd}
+        >
+          <Button variant="default" px="xs">
+            Log in
+          </Button>
+          <Button px="xs">Sign up</Button>
+        </Group>
+        {/* <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" /> */}
+      </Container>
+    </header>
+  );
+}
