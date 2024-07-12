9e41c26fdc9c9a9c431c384e8b3dc27e47312d6d
cechua <chuacelc08@gmail.com>
2024-07-12 19:52:06 +0800

    Add Job Details by id page.

--- src/components/jobDetails/JobDetailSummary.tsx
+++ src/components/jobDetails/JobDetailSummary.tsx
@@ -1,7 +1,74 @@
-import React from 'react';
+import {
+  Group,
+  Stack,
+  Image,
+  Text,
+  Divider,
+  Flex,
+  Chip,
+  Button,
+  Container,
+} from '@mantine/core';
+import LabelValue from '../common/labelValue/LabelValue';
 
-const JobDetailSummary = () => {
-  return <div>JobDetailSummary</div>;
+interface JobDetailSummaryProps {
+  companyImage: string;
+  companyName: string;
+  jobPosition: string;
+  jobLocation: string;
+}
+
+const JobDetailSummary = ({
+  companyImage,
+  companyName,
+  jobLocation,
+  jobPosition,
+}: JobDetailSummaryProps) => {
+  return (
+    <Stack>
+      <Group>
+        <Image src={companyImage} radius={100} w={90} h={90} />
+        <Stack gap={2}>
+          <Text tt="uppercase" c="solid" fw={700} size="lg">
+            {jobPosition}
+          </Text>
+          <Text fw={400} size="md">
+            {companyName}
+          </Text>
+          <Text c="dimmed" fw={400} size="xs">
+            {jobLocation}
+          </Text>
+          <Flex pt={4} gap={8}>
+            <Chip size="xs">Full-time</Chip>
+            <Chip size="xs">Hybrid</Chip>
+            <Chip size="xs">Php100,000</Chip>
+          </Flex>
+        </Stack>
+      </Group>
+      <Divider />
+      <Stack>
+        <Text tt="uppercase" c="solid" fw={600} size="md">
+          Requirements:
+        </Text>
+        <LabelValue
+          label="Education"
+          value="Bacholor of Science in Computer science or equivalent"
+        />
+        <LabelValue
+          label="Skills"
+          value="ReactJS, Typescript, NodeJS, Docker, AWS, PostgreSQL"
+        />
+        <Group>
+          <Button>Apply</Button>
+          <Button>Save</Button>
+        </Group>
+      </Stack>
+      <Divider />
+      <Container>
+        JOB DESCRIPTION ALL HERE. TEST ONCE RICH TEXT INPUT IS READY
+      </Container>
+    </Stack>
+  );
 };
 
 export default JobDetailSummary;
--- src/components/jobDetails/JobDetails.tsx
+++ src/components/jobDetails/JobDetails.tsx
@@ -1,24 +1,45 @@
-import { Container, Grid, GridCol, Stack } from '@mantine/core';
+import { Container, Flex, Group, Stack } from '@mantine/core';
 import { useSearchParams } from 'react-router-dom';
 import classes from './jobDetails.module.css';
 import JobCardSummary from '../jobCard/JobCardSummary';
+import JobDetailSummary from './JobDetailSummary';
 
 const JobDetails = () => {
   const [searchParams] = useSearchParams();
   const jobId = searchParams.get('jobId') ?? '';
   return (
-    <Container className={classes.container} fluid>
-      <Grid>
-        <GridCol span={{ md: 5, xl: 4 }} visibleFrom="md">
-          <Stack>
+    <Container className={classes.container} size="xl">
+      <Group align="flex-start">
+        <Stack className={classes.jobSummaryContainer}>
+          <Flex visibleFrom="md" direction="column" gap={8}>
             <JobCardSummary />
             <JobCardSummary />
-          </Stack>
-        </GridCol>
-        <GridCol span={{ md: 7, xl: 8 }}>
-          <Stack bg="red">{jobId}</Stack>
-        </GridCol>
-      </Grid>
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+            <JobCardSummary />
+          </Flex>
+        </Stack>
+        <Container fluid m={0} className={classes.jobDetailContainer}>
+          <JobDetailSummary
+            companyImage="https://images.unsplash.com/photo-1602080858428-57174f9431cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80"
+            companyName="Company A"
+            jobLocation="Taguig, Philippines"
+            jobPosition="Senior Fullstack Developer"
+          />
+          {jobId}
+        </Container>
+      </Group>
     </Container>
   );
 };
